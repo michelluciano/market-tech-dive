7c9d9070a56013418e851f7b35e8eb1a57dab89b
Marcos Michel Luciano <56523274+michelluciano@users.noreply.github.com>
2022-03-16 20:54:16 -0300

    commit product

---
+++ src/main/java/market/model/persistence/Product.java
@@ -0,0 +1,91 @@
+package market.model.persistence;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, length = 64)
+	private String name;
+	
+	@Column(nullable = false)
+	private String description;
+	
+	@Column(nullable = false)
+	private BigDecimal price;
+	
+	@OneToMany()
+	private Category category;
+	
+	public Product() {
+		
+	}
+
+	public Product(long id, String name, String description, BigDecimal price, Category category) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.category = category;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
+	}
+	
+	
+	
+}
